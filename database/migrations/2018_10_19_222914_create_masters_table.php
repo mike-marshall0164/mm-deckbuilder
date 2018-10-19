9420956fefa6951e481392462f9f624a8274c78e
Mike Marshall <onewingedangel0164@gmail.com>
2018-10-19 16:13:21 -0700

    create placeholder for cards table and a first draft for masters table

---
+++ database/migrations/2018_10_19_222914_create_masters_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMastersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('masters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string("name");
+            $table->string("description");
+            $table->string("extra_data");
+            $table->decimal("range", 5, 2);
+            $table->decimal("attack_damage", 5, 2);
+            $table->decimal("target_count", 4, 2);
+            $table->decimal("attack_speed", 4, 2);
+            $table->decimal("attack_delay", 4, 2);
+            $table->decimal("aoe_size", 5, 2)->nullable();
+            $table->boolean("can_hit_air");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('masters');
+    }
+}
