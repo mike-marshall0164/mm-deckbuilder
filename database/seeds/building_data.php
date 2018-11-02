46f838d7245ef6b3ce3b1008e6e514222281ebf6
Mike Marshall <onewingedangel0164@gmail.com>
2018-11-02 14:41:14 -0700

    #4 add seeds for cards

---
+++ database/seeds/building_data.php
@@ -0,0 +1,175 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class building_data extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('cards')->insert([
+            "name" => "Wall",
+            "description" => "Plenty of health, no attack. A perfect distraction for building haters",
+            "mana_cost" => 2,
+            "unit_health" => 600,
+            "duration" => 20,
+            "unit_range" => "",
+            "damage" => "",
+            "attack_speed" => "",
+            "attack_delay" => "",
+            "unit_production_speed" => "",
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "can_target_air" => "",
+            "extra_info" => ""
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Ghost Turret",
+            "description" => "Defensive building with good damage and attack speed.",
+            "mana_cost" => 3,
+            "unit_health" => 250,
+            "duration" => 30,
+            "unit_range" => 6,
+            "damage" => 50,
+            "attack_speed" => 0.8,
+            "attack_delay" => 0.4,
+            "unit_production_speed" => "",
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "can_target_air" => false,
+            "extra_info" => ""
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Healing Shrine",
+            "description" => "Heals all nearby friendly minions over time",
+            "mana_cost" => 3,
+            "unit_health" => 50,
+            "duration" => 12,
+            "unit_range" => 8,
+            "damage" => "",
+            "attack_speed" => 1,
+            "attack_delay" => "",
+            "unit_production_speed" => "",
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "can_target_air" => "",
+            "extra_info" => "The healing is an ability so Stormbringer doesn't increase the shrine's range"
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Bridge Shrine",
+            "description" => "Generates 15 XP over 60 sec - regards, XP INC. When in Mana Frenzy the shrine overheats and takes triple decay damage.",
+            "mana_cost" => 4,
+            "unit_health" => 250,
+            "duration" => 60,
+            "unit_range" => "",
+            "damage" => "",
+            "attack_speed" => "",
+            "attack_delay" => "",
+            "unit_production_speed" => 4,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "can_target_air" => "",
+            "extra_info" => "For reference, a regular bridge generates 1 XP per 4 sec. aswell."
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Magma Cannon",
+            "description" => "This cannon fires flaming balls of magma at any enemies in range. For when you need more firepower.",
+            "mana_cost" => 4,
+            "unit_health" => 300,
+            "duration" => 30,
+            "unit_range" => 8,
+            "damage" => 75,
+            "attack_speed" => 1.2,
+            "attack_delay" => 0.6,
+            "unit_production_speed" => "",
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "can_target_air" => true,
+            "extra_info" => ""
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Dragon Nest",
+            "description" => "Summons a Flightless Dragon Whelp whenever its Master plays a Spell. Because dragons love magic.",
+            "mana_cost" => 5,
+            "unit_health" => 300,
+            "duration" => 45,
+            "unit_range" => "",
+            "damage" => "",
+            "attack_speed" => "",
+            "attack_delay" => "",
+            "unit_production_speed" => "",
+            "rarity" => "Legendary",
+            "crafting_cost" => 2000,
+            "can_target_air" => "",
+            "extra_info" => ""
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Laser Turret",
+            "description" => "When staying on the same target, it fires faster and faster.",
+            "mana_cost" => 5,
+            "unit_health" => 350,
+            "duration" => 30,
+            "unit_range" => 11,
+            "damage" => 40,
+            "attack_speed" => 1.4,
+            "attack_delay" => 1,
+            "unit_production_speed" => "",
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "can_target_air" => true,
+            "extra_info" => "Reduces its attack speed by 300 ms every time it attacks the same target. (Max's out at a minimum of 100ms)"
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Scrat Launcher",
+            "description" => "Massive range, always shoots at the nearest enemy building. Scrats go in; screaming, rocket-powered balls of fur come out.",
+            "mana_cost" => 5,
+            "unit_health" => 300,
+            "duration" => 30,
+            "unit_range" => 25,
+            "damage" => 100,
+            "attack_speed" => 3,
+            "attack_delay" => 0.4,
+            "unit_production_speed" => "",
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "can_target_air" => "",
+            "extra_info" => ""
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Swarmer Totem",
+            "description" => "Summons a lot of Swarmers over time.",
+            "mana_cost" => 5,
+            "unit_health" => 350,
+            "duration" => 60,
+            "unit_range" => "",
+            "damage" => "",
+            "attack_speed" => "",
+            "attack_delay" => "",
+            "unit_production_speed" => 6.6,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "can_target_air" => "",
+            "extra_info" => ""
+          ]);
+          DB::table('cards')->insert([
+            "name" => "Crossbow Guild",
+            "description" => "Place a guild of jolly Crossbowmen, try to give them enough time to start a community.",
+            "mana_cost" => 6,
+            "unit_health" => 350,
+            "duration" => 60,
+            "unit_range" => "",
+            "damage" => "",
+            "attack_speed" => "",
+            "attack_delay" => "",
+            "unit_production_speed" => 4.2,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "can_target_air" => "",
+            "extra_info" => ""
+          ]);
+    }
+}
---
+++ database/seeds/spell_data.php
@@ -0,0 +1,393 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class spell_data extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {                
+        DB::table('cards')->insert([
+            "name" => "Black Hole",
+            "description" => "Sucks up ALL minions in an area, removing them from the board for the rest of the spell's duration",
+            "mana_cost" => 2,
+            "unit_count" => "",
+            "radius" => 5,
+            "damage" => "",
+            "duration" => 6,
+            "delay" => 1,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => "Black Holed units do not count towards conditional cards' activation requirements."
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Bridge Buddies",
+            "description" => "Summon a Scrat on each bridge. If the bridge is under your control, summon an Armored Scrat instead.",
+            "mana_cost" => 2,
+            "unit_count" => 2,
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 2,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Call To Arms",
+            "description" => "Replace each of your buildings with a Warrior.",
+            "mana_cost" => 2,
+            "unit_count" => "",
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 2,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => "There's a slight delay. Any buildings destroyed during the delay will not be replaced by Call To Arms."
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Combustion",
+            "description" => "Each affected Friendly Minion explodes after 3 seconds, dealing 150 damage to itself and any nearby Enemies.",
+            "mana_cost" => 2,
+            "unit_count" => "",
+            "radius" => 3,
+            "damage" => 150,
+            "duration" => 3,
+            "delay" => 1.5,
+            "rarity" => "Legendary",
+            "crafting_cost" => 2000,
+            "extra_info" => "Hits ALL enemies around the Combustion'd Minion. "
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Shock Rock",
+            "description" => "Drop a stunning stone on your enemies, which stuns and damages them.",
+            "mana_cost" => 2,
+            "unit_count" => "",
+            "radius" => 3,
+            "damage" => 40,
+            "duration" => 2,
+            "delay" => 1.5,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Cheese Date",
+            "description" => "Spawn a random 3 mana scrat card and give it Rage",
+            "mana_cost" => 3,
+            "unit_count" => 1,
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 1.35,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => "Possible minions: Scrat Horde, Sniper Scrat, Bazooka Scrat, Armored Scrats"
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Clear Skies",
+            "description" => "A soothing wind clears the arena healing all minions and masters 250 health and removes rage and shields.",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => "",
+            "damage" => 100,
+            "duration" => "",
+            "delay" => 0,
+            "rarity" => "Legendary",
+            "crafting_cost" => 2000,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Daggerfall",
+            "description" => "Great against hordes of low health enemies, such as Scrats!",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => 8,
+            "damage" => 80,
+            "duration" => "",
+            "delay" => 2,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Gambler's Ball",
+            "description" => "Casts either a Healing Fireball or a Fireball at the targeted area. Like a certain serpent once said; Luck iss a ssskill",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 2.5,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "extra_info" => "Stats of this card are decided by which spell it casts. Refer to Healing Fireball(3 Mana) and Fireball(4 Mana) for detailed statistics."
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Gor'Rakk Sacrifice",
+            "description" => "Disables a random friendly Minion for 5 seconds, then kills it to spawn a Cleaver. Keep the sacrifice alive or lose it!",
+            "mana_cost" => 3,
+            "unit_count" => 1,
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 0.5,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Healing Fireball",
+            "description" => "Heals all friendly minions by 400. Yes, it is a HEALING fireball...",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => 6,
+            "damage" => -400,
+            "duration" => "",
+            "delay" => 2.5,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Netherstep",
+            "description" => "Teleports friendly Minions in the area along the path to the enemy Master. The Shars'Rakk love sharing their magic - and other things...",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => 3,
+            "damage" => "",
+            "duration" => "",
+            "delay" => 0.5,
+            "rarity" => "Legendary",
+            "crafting_cost" => 2000,
+            "extra_info" => "Teleport Distance: 10"
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Rampage",
+            "description" => "Gives friendly Minions Rage! +50%",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => 8,
+            "damage" => "",
+            "duration" => "",
+            "delay" => 2.5,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Stun Blast",
+            "description" => "A large stun explosion, that leaves your enemies vulnerable.",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => 5,
+            "damage" => "",
+            "duration" => 5,
+            "delay" => 1.5,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Sun Burn",
+            "description" => "Deal 10 damage 10 times to all units in a small area and give them Rage (+50% Damage)",
+            "mana_cost" => 3,
+            "unit_count" => "",
+            "radius" => 3,
+            "damage" => "",
+            "duration" => 1,
+            "delay" => 1,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => "Can hit your own minions and masters!"
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Blind Date",
+            "description" => "Play a random 5 mana minion card.",
+            "mana_cost" => 4,
+            "unit_count" => 1,
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 1.35,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Chain Lightning",
+            "description" => "Fires a ball of lightning at target location before bouncing to the nearest enemies, damaging them and stunning them for 0.5 seconds, until there are none within range.",
+            "mana_cost" => 4,
+            "unit_count" => "",
+            "radius" => 8,
+            "damage" => 100,
+            "duration" => 0.5,
+            "delay" => 2.5,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Fireball",
+            "description" => "Great against groups of low to medium health enemies.",
+            "mana_cost" => 4,
+            "unit_count" => "",
+            "radius" => 4.5,
+            "damage" => 150,
+            "duration" => "",
+            "delay" => 2.5,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Future Past",
+            "description" => "When used, it permanently replaces itself with another random card of cost 1 or higher and reduces that card's mana cost by 1.",
+            "mana_cost" => 4,
+            "unit_count" => "",
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 0.5,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => "Can not draw Future Past or Future Present"
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Hypnotize",
+            "description" => "After a short delay, enemy minions caught in the area are hypnotized and join your side for 5 seconds.",
+            "mana_cost" => 4,
+            "unit_count" => "",
+            "radius" => 4.5,
+            "damage" => "",
+            "duration" => 5,
+            "delay" => 2.5,
+            "rarity" => "Legendary",
+            "crafting_cost" => 2000,
+            "extra_info" => "Hypnotised units count towards conditional card' activation requirements."
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Lightning Bolt",
+            "description" => "Destroys one random enemy Minion or Building.",
+            "mana_cost" => 4,
+            "unit_count" => "",
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 0.999,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Raging Reinforcements",
+            "description" => "Summons a Dragon Whelp or a Warrior or 3 Crossbow Dudes and gives them Rage! +50% Damage",
+            "mana_cost" => 4,
+            "unit_count" => "",
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 0.5,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Spirit Infusion",
+            "description" => "Summons 3 Spirits over 9 seconds. Each Spirit flies to a random friendly Minion giving it 100 extra health",
+            "mana_cost" => 4,
+            "unit_count" => 3,
+            "radius" => "",
+            "damage" => "",
+            "duration" => 9,
+            "delay" => 1.5,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Dragon Ball",
+            "description" => "It's a fireball, but if the last card you played was a 3-mana minion, it also spawns a Dragon Whelp where it lands!",
+            "mana_cost" => 5,
+            "unit_count" => 1,
+            "radius" => 4.5,
+            "damage" => 150,
+            "duration" => "",
+            "delay" => 2.5,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Infiltration",
+            "description" => "Summon 4 Plasma Marines wherever you want!",
+            "mana_cost" => 5,
+            "unit_count" => 4,
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 1.5,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Magma Storm",
+            "description" => "Calls down 10 magma rocks in an area over 5 sec. Each rock deals 200 damage to ANYTHING in a small area (but only 10% to Masters)",
+            "mana_cost" => 5,
+            "unit_count" => "",
+            "radius" => 9,
+            "damage" => 200,
+            "duration" => 5,
+            "delay" => 1.5,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => "Summons 2 rocks per second that deal 200 Damage each in a radius of 3.5 range each, only 20 damage each to Masters. Can hit your own minions and master!"
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Beam of DOOM!",
+            "description" => "A popular UFO weapon, deals massive damage in a small area over a few seconds, Double damage against minions.",
+            "mana_cost" => 7,
+            "unit_count" => "",
+            "radius" => 3,
+            "damage" => 300,
+            "duration" => 2.8,
+            "delay" => 1.2,
+            "rarity" => "Common",
+            "crafting_cost" => 50,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Last Stand",
+            "description" => "Summons 5 Legionnaires, but 7 if your opponent controls both bridges!",
+            "mana_cost" => 7,
+            "unit_count" => 6,
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 0.5,
+            "rarity" => "Rare",
+            "crafting_cost" => 125,
+            "extra_info" => ""
+        ]);
+        DB::table('cards')->insert([
+            "name" => "Future Present",
+            "description" => "When used, it permanently replaces itself with another random card of cost 2 or higher and reduces that card's mana cost by 2.",
+            "mana_cost" => 8,
+            "unit_count" => "",
+            "radius" => "",
+            "damage" => "",
+            "duration" => "",
+            "delay" => 0.5,
+            "rarity" => "Supreme",
+            "crafting_cost" => 500,
+            "extra_info" => "Can not draw Future Past or Future Present"
+            ]);
+
+    }
+}
